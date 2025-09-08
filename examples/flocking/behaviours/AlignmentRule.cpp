642b459ebc41a5de488a5c359ec78b4f817e23e7
Fable-Spagat <fable.spagat@mymail.champlain.edu>
2025-09-08 11:13:54 -0400

    Alignment and attempt at cohesion.

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -7,6 +7,11 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
 
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
+  for (int i = 0; i < neighborhood.size(); i++) {
+    averageVelocity += neighborhood[i]->velocity;
+  }
 
-  return Vector2f::normalized(averageVelocity);
+  averageVelocity /= neighborhood.size();
+
+  return Vector2f::normalized(averageVelocity) * weight;
 }
--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -2,11 +2,25 @@
 #include "../gameobjects/Boid.h"
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
-  Vector2f cohesionForce;
+  Vector2f cohesionForce = {0,0};
 
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
 
+  for (int i = 0; i < neighborhood.size(); i++) {
+    if (neighborhood[i] != boid)
+      cohesionForce += neighborhood[i]->getPosition();
+  }
+
+  if (neighborhood.size() - 1 == 0) {
+    cohesionForce = {0,0};
+  }
+  else {
+    cohesionForce /= neighborhood.size() - 1;
+    cohesionForce /= boid->getDetectionRadius();
+    cohesionForce -= boid->getPosition();
+    cohesionForce *= weight;
+  }
   // find center of mass
 
   return cohesionForce;
