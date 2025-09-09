70a626fba7d075638c713e2a6b3703139c3b3134
fable.spagat <fable.spagat@mymail.champlain.edu>
2025-09-09 18:31:38 -0400

    2 extra behaviours.

--- examples/flocking/behaviours/WindRule.cpp
+++ examples/flocking/behaviours/WindRule.cpp
@@ -5,9 +5,16 @@
 #include "engine/Engine.h"
 
 Vector2f WindRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
+  Vector2f windForce = {0, 0};
+
   // todo: add a wind force here
   // hint: use the windAngle variable
-  return Vector2f::zero();
+
+  windForce.x = cos(windAngle);
+  windForce.y = sin(windAngle);
+  windForce *= weight;
+
+  return windForce * 1000;
 }
 
 bool WindRule::drawImguiRuleExtra() {
